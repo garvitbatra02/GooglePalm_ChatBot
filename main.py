9806e171eb02a01fc8d67476f468ff257272cfc0
Garvit Batra <gbatra145@gmail.com>
2024-01-09 20:24:29 +0530

    sqlite3 changes added

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from langchain_helper import get_qa_chain, create_vector_db
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 st.title("Garvit Batra Q/A bot🌱")
 btn = st.button("Create Knowledgebase")
